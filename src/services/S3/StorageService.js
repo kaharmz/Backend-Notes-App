e6517afd8c38a3c0d3cf29af777a8a4bd61c67fc
kaharmz <abdkaharmuzakir@gmail.com>
2021-09-10 22:17:44 +0700

    S3 storage implemented

---
+++ src/services/S3/StorageService.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-underscore-dangle */
+
+const AWS = require('aws-sdk');
+
+class StorageService {
+  constructor() {
+    this._S3 = new AWS.S3();
+  }
+
+  writeFile(file, meta) {
+    const parameter = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      key: +new Date() + meta.fileName,
+      Body: file._data,
+      ContentType: meta.headers['content-type'],
+    };
+    return new Promise((resolve, reject) => {
+      this._S3.upload(parameter, (error, data) => {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(data.Location);
+      });
+    });
+  }
+}
+
+module.exports = StorageService;
